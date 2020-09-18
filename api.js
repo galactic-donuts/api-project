52318fcb9c552a933eccd5206726514c25c66934
Kiya Jones <gdragonswaggin@gmail.com>
2020-09-17 22:30:33 -0400

    update

--- api.js
+++ api.js
@@ -1,15 +1,16 @@
-const baseURL = 'http://ghibliapi.herokuapp.com';
-//site says that no authentication is required to use api so no key is necessary
 let url = 'https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49';
 
-const ghibli = document.getElementById ('.ghibli')
-const press = document.getElementsByClassName ('press')
-press.addEventListener ('click', fetchResults);
+const ghibli = document.getElementById('ghibli');
+const button = document.getElementById('press');
+button.addEventListener('click', fetchResult);
 
-function fetchResults () {
-fetch('https://ghibliapi.herokuapp.com/films') 
-.then (res => res.json())
-.then (res => fetchResults);
+function fetchResult() {
+  fetch(url)
+  	// This line converts it to a usable JSON object
+    .then(response => response.json()) 
+    // This line appends the title of the result to the page
+    .then(result => {
+        //Should change 'Studio Ghibli' to 'My Neighbor Totoro' when 'Movie' button is clicked 
+      ghibliDirectory.innerHTML = result.title;
+    });
 }
-
-  
